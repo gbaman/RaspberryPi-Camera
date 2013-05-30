8c65c69f1ca333f07a733c830fccb472d746f081
gbaman <gbaman1@gmail.com>
2013-05-30 18:27:50 +0100

    Updated panobot

--- Panobot/Panobot.py
+++ Panobot/Panobot.py
@@ -1,4 +1,5 @@
-# By Andrew Mulholland aka gbaman
+#Panobot v0.2
+#By Andrew Mulholland aka gbaman
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
